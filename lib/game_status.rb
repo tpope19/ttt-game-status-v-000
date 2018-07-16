71af42ccaf075b53f0d8b686cd29ee1fa6c9bf45
Thomas Pope <tpope0928@gmail.com>
2018-07-16 00:01:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,11 @@ WIN_COMBINATIONS = [
   [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
 ]
 
-
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combination|
+    board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
+  end
+end
 
 def full?(board)
   board.none? do |i|
