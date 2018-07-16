3564332f5c69be5c1ca0655db02d7ed162da178d
Thomas Pope <tpope0928@gmail.com>
2018-07-16 00:17:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,55 +1,57 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
-]
-
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return win_combo
     end
-
+      false
   end
-else
-  false
 end
 
+#full?
 def full?(board)
-  board.none? do |i|
-    i == " " || i.nil?
-  end
+  board.all?{|occupied| occupied != " "}
 end
 
+#draw?
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  elsif !won?(board) && !full?(board) || !won?(board)
-    return false
-  end
+  !(won?(board)) && (full?(board))
 end
 
-draw?(board)
-
+#over?
 def over?(board)
-  draw(board) == true || won?(board) != nil
+  (won?(board)) || (full?(board)) || (draw?(board))
 end
 
+#winner?
 def winner(board)
-  if won?(board) != nil
-    winner = board[won?(board)[0]]
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return "X"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
   end
 end
+# passed 5/6
