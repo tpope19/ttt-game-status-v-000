00cb1c076ee88b8077effe2f8b8cd56174245a2d
Thomas Pope <tpope0928@gmail.com>
2018-07-16 00:13:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,13 +10,22 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |winner_set|
-    if board[winner_set[0]] == board[winner_set[1]] && board[winner_set[1]] == board[winner_set[2]] && position_taken?(board, winner_set[0])
-      puts "Winner!"
-      return true
-      return winner_set
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     end
+
   end
+else
+  false
 end
 
 def full?(board)
