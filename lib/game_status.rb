7de3b349328260b970591c1f86b583b73908e49f
Thomas Pope <tpope0928@gmail.com>
2018-07-16 00:17:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
     [0,4,8],
     [2,4,6]
   ]
-  
+
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
